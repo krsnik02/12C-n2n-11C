1db3ea681c2f5e8c7d5d043c7029330682ca2675
Garrett Hartshaw <ghartshaw@gmail.com>
2013-06-10 14:25:45 -0400

    Calculate neutron flux by elastic scattering

--- summary.cxx
+++ summary.cxx
@@ -52,12 +52,12 @@ enum RunSummary {
 	RS_ROI_YMAX,		///< Max Y value for region of interest
 	RS_GROSS_PROTONS,	///< Gross protons detected (counts)
        	RS_NET_PROTONS,		///< Net protons detected (counts)
-        RS_C11_PLASTIC_COUNT,	///< Calculated C11 decays in CH2 (counts)
-       	RS_C11_PLASTIC_ERROR,	///< Error in CSV_CH2_C11_COUNT
-       	RS_C11_PUCK_COUNT,	///< Calculated C11 decays in graphite (counts)
-        RS_C11_PUCK_ERROR,	///< Error in CSV_GRAPHITE_C11_COUNT
+        RS_C11_PLASTIC,		///< Calculated C11 decays in CH2 (counts)
+       	RS_C11_PLASTIC_ERR,	///< Error in RS_C11_PLASTIC
+       	RS_C11_PUCK,		///< Calculated C11 decays in graphite (counts)
+        RS_C11_PUCK_ERR,	///< Error in RS_C11_PUCK
        	RS_PROTONS_COUNT,	///< Protons detected (counts/&mu;C)
-	RS_PROTONS_ERROR,	///< Error in CSV_PROTONS_COUNT
+	RS_PROTONS_ERROR,	///< Error in RS_PROTONS
 	RS_PROTONS_PER_NEUTRON,	///< Protons detected (counts/neutron)
         RS_DISTANCES,		///< Distance Section Header
        	RS_CH2_DISTANCE,	///< Distance to front of CH2 (cm)
@@ -75,11 +75,21 @@ enum RunSummary {
 enum CrossSection {
 	CS_FG_RUN_NUMBER,	///< Run number of foreground run
 	CS_BG_RUN_NUMBER,	///< Run number of background run
-	CS_ENERGY,		///< Neutron energy for both runs (MeV)
-	CS_FG_PROTONS,		///< Number of protons in foreground run (#)
-	CS_BG_PROTONS,		///< Number of protons in background run (#)
+	CS_NEUTRON_ENERGY,	///< Neutron energy for both runs (MeV)
+	CS_BEAM_CHARGE,		///< Total beam charge (&mu;C) 
+	CS_ELAPSED_TIME,	///< Total run time (s)
 	CS_FG_LIVE_TIME,	///< Live time of foreground run (s)
 	CS_BG_LIVE_TIME,	///< Live time of background run (s)
+	CS_C11_PLASTIC,		///< Calculated decays in CH2 (counts)
+	CS_C11_PLASTIC_ERR,
+	CS_C11_PUCK,		///< Calculated decays in graphite (counts)
+	CS_C11_PUCK_ERR,	
+	CS_FG_PROTONS,		///< Number of protons in foreground run (#)
+	CS_BG_PROTONS,		///< Number of protons in background run (#)
+	CS_PROTONS,		///< Net protons (background subtracted) (#/s)
+	CS_PROTONS_ERR,
+	CS_NEUTRON_FLUX,	///< Measured neutron flux (1/s sr)
+	CS_NEUTRON_FLUX_ERR,
 	CS_NUM_COLUMNS
 };
 
@@ -258,9 +268,16 @@ void UpdateFG( std::vector<std::string> & row, char const * filename )
 			break;
 	}
 
-	row[CS_ENERGY] = fg[RS_NEUTRON_ENERGY];
+	row[CS_NEUTRON_ENERGY] = fg[RS_NEUTRON_ENERGY];
+	row[CS_BEAM_CHARGE] = fg[RS_TOTAL_CHARGE];
 	row[CS_FG_PROTONS] = fg[RS_GROSS_PROTONS];
 	row[CS_FG_LIVE_TIME] = fg[RS_LIVE_TIME];
+	row[CS_ELAPSED_TIME] = fg[RS_CLOCK_TIME];
+
+	row[CS_C11_PLASTIC] = fg[RS_C11_PLASTIC];
+	row[CS_C11_PLASTIC_ERR] = fg[RS_C11_PLASTIC_ERR];
+	row[CS_C11_PUCK] = fg[RS_C11_PUCK];
+	row[CS_C11_PUCK_ERR] = fg[RS_C11_PUCK_ERR];
 }
 
 void UpdateBG( std::vector<std::string> & row, char const * filename )
@@ -286,6 +303,96 @@ void UpdateBG( std::vector<std::string> & row, char const * filename )
 
 	row[CS_BG_PROTONS] = bg[RS_GROSS_PROTONS];
 	row[CS_BG_LIVE_TIME] = bg[RS_LIVE_TIME];
+}
+
+
+
+
+
+/**
+ * Determine the cross section of a (n,p) reaction based on a 
+ * cspline interpolation between known cross sections.
+ *
+ * @param energy the kinetic energy of the reaction (MeV)
+ *
+ * @return the cross section at that energy (mb/sr)
+ */
+double GetCrossSection_np( double energy )
+{
+	double energies[] = { 20, 22, 24, 26, 28 }; 	// (MeV)
+	double x_sects[] = { 153, 139, 128, 119, 111 }; // (mb/sr)
+
+	ROOT::Math::Interpolator interp( 5 );
+	interp.SetData( 5, energies, x_sects );
+	return interp.Eval( energy );
+}
+
+/** 
+ * Get the hydrogen number thickness in a specified thickness of CH2.
+ * @f$N_{CH2}(\mathrm{thickness})=\frac{2\rho_{CH2}}{m_{CH2}}\cdot\mathrm{thickness}@f$
+ *
+ * @param thickness the thickness of the CH2 (cm)
+ *
+ * @return the hydrogen number thickness in that amount of CH2 (protons/cm^2)
+ */
+double GetNumberThickness_H_CH2( double thickness )
+{
+	double mass_H = 1.007825; 		// u
+	double mass_C = 12;			// u
+	double mass_CH2 = 2 * mass_H + mass_C;	// u
+	double density_CH2 = 0.89; 		// g/cm^3
+	
+	// 1 u = 1.660538782e-24 g
+	return 2 * thickness * density_CH2 / (1.660538782e-24 * mass_CH2);	// protons/cm^2
+}
+
+/**
+ * Get the solid angle of an object.
+ * @f$\Omega(\mathrm{area},\mathrm{distance})=\frac{\mathrm{area}}{\mathrm{distance}^2}@f$
+ *
+ * @param area the cross sectional area of the target (cm^2)
+ * @param distance the distance of the target (cm)
+ *
+ * @return the solid angle of the target (sr)
+ */
+double GetSolidAngle( double area, double distance )
+{
+	return area / pow( distance, 2 );
+}
+
+void UpdateNeutronFlux( std::vector<std::string> row )
+{
+	int fg_protons = atoi( row[CS_FG_PROTONS].c_str() );			// protons
+	int bg_protons = atoi( row[CS_BG_PROTONS].c_str() );			// protons
+	double fg_live = atof( row[CS_FG_LIVE_TIME].c_str() );			// s
+	double bg_live = atof( row[CS_BG_LIVE_TIME].c_str() );			// s
+
+	double protons = (fg_protons / fg_live) - (bg_protons / bg_live);	// protons/s
+	double protons_err = sqrt( pow( sqrt( fg_protons ) / fg_live, 2 ) +
+			pow( sqrt( bg_protons ) / bg_live, 2 ) );		// protons/s
+	
+	double energy = atof( row[CS_NEUTRON_ENERGY].c_str() );			// MeV
+	double thick_CH2 = 0.164; 						// cm
+	double area_E = 0.7133;							// cm^2
+	double dist_E = 12;							// cm
+	double area_CH2 = 5.06707479;						// cm^2
+	double dist_CH2 = 6.5;							// cm
+
+	double cross_section_np = GetCrossSection_np( energy );			// mb/sr
+	double num_thick_CH2 = GetNumberThickness_H_CH2( thick_CH2 );		// protons/cm^2
+	double solid_angle_E = GetSolidAngle( area_E, dist_E );			// sr
+	double solid_angle_CH2 = GetSolidAngle( area_CH2, dist_CH2 );		// sr
+
+	// 1 mb = 1e-27 cm^2
+	double nflux = protons / (cross_section_np * 1e-27
+			* num_thick_CH2 * solid_angle_E * solid_angle_CH2 );	// protons/s*sr
+	double nflux_err = protons_err / (cross_section_np * 1e-27
+			* num_thick_CH2 * solid_angle_E * solid_angle_CH2 );	// protons/s*sr
+
+	row[CS_PROTONS] = TString::Format( "%f", protons );
+	row[CS_PROTONS_ERR] = TString::Format( "%f", protons_err );
+	row[CS_NEUTRON_FLUX] = TString::Format( "%f", nflux );
+	row[CS_NEUTRON_FLUX_ERR] = TString::Format( "%f", nflux_err );	
 }
 
 
@@ -319,6 +426,9 @@ void UpdateCrossSections( char const * dirname )
 		std::vector<std::string> row = CSVParseRow( line );
 		UpdateFG( row, gSystem->ConcatFileName( dirname, "Run_Summary.csv" ) );
 		UpdateBG( row, gSystem->ConcatFileName( dirname, "Run_Summary.csv" ) );
+
+		UpdateNeutronFlux( row );
+
 		ofs << CSVFormatRow( row ) << std::endl;
 	}
 	
