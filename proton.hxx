af22b1c86f032293f15b7b40f3b4d2e54a411571
Garrett Hartshaw <ghartshaw@gmail.com>
2013-06-17 16:33:31 -0400

    Added doxygen comments to proton.hxx.

--- proton.hxx
+++ proton.hxx
@@ -7,16 +7,39 @@
 
 namespace n2n {
 
+/**
+ * A region of interest.
+ */
 struct Region
 {
-	Int_t min_x, max_x;
-	Int_t min_y, max_y;
+	Int_t min_x; 	///< The smallest x-value in the region.
+	Int_t max_x;	///< The largest x-value in the region.
+	Int_t min_y;	///< The smallest y-value in the region.
+	Int_t max_y;	///< The largest y-value in the region.
 };
 
 namespace proton {
 
+/**
+ * Parse the .csv data file produced by MPA4 for the proton telescope.
+ * @param filename The path to the file.
+ * @return A 2D-histogram containing the data as a dE-E plot.
+ */
 TH2I * ParseDataFile( char const * const filename );
+
+/**
+ * Parse the .mpa file produced by MPA4 for the proton telescope.
+ * @param filename The path to the file.
+ * @return The region of interest for the run.
+ */
 Region ParseHeaderFile( char const * const filename );
+
+/**
+ * Determine the total number of counts in the region of interest.
+ * @param data The dE-E data.
+ * @param roi The region of interest.
+ * @return The number of counts in the region.
+ */
 Int_t CountsInRegion( TH2I const * const data, Region const & roi );
 
 } // namespace proton
